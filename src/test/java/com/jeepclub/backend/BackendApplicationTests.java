45a90936ae385ffd0796eca7cae7533665752aac
Luskahz <lucasalvesdemelolins@gmail.com>
2026-03-23 00:04:19 -0300

    [feat] - inicializando spring boot

---
+++ src/test/java/com/jeepclub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeepclub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
