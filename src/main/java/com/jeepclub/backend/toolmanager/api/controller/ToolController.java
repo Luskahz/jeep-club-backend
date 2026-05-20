5a61af1d2a8435bf37ff4efd604eec337f299e08
Lushakz <lucasalvesdemelolins@gmail.com>
2026-05-19 21:32:40 -0300

    [fix] - ajustei uma linha, e coloquei umas observações.

--- src/main/java/com/jeepclub/backend/toolmanager/api/controller/ToolController.java
+++ src/main/java/com/jeepclub/backend/toolmanager/api/controller/ToolController.java
@@ -4,6 +4,7 @@ import com.jeepclub.backend.toolmanager.api.dto.ToolResponseDTO;
 import com.jeepclub.backend.toolmanager.core.service.ToolQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +13,15 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/tools")
+@RequiredArgsConstructor
 @Tag(name = "Tool Manager - Public", description = "Consulta de ferramentas e equipamentos disponíveis.")
 public class ToolController {
 
     private final ToolQueryService toolQueryService;
 
-    public ToolController(ToolQueryService toolQueryService) {
-        this.toolQueryService = toolQueryService;
-    }
 
+    // atenção, essa rota está trazendo todas as tools do banco, se esse for o intuito isso tem que ser uma rota administrativa bloqueada
+    // use o @AuthenticationPrincipal para descobrir qual usuario está fazendo esta requisição e filtrar para visualização apenas das ferramentas dele
     @GetMapping
     @Operation(summary = "Listar ferramentas disponíveis", description = "Retorna todas as ferramentas que estão prontas para uso/empréstimo.")
     public ResponseEntity<List<ToolResponseDTO>> getAvailableTools() {
@@ -30,11 +31,16 @@ public class ToolController {
                 .collect(Collectors.toList());
         return ResponseEntity.ok(tools);
     }
-
+    // mesma observação: qualquer um que chamar essa rota pode ver as ferramentas de qualquer um
     @GetMapping("/{id}")
     @Operation(summary = "Buscar detalhes da ferramenta", description = "Retorna os dados de uma ferramenta específica pelo ID.")
     public ResponseEntity<ToolResponseDTO> getToolById(@PathVariable Long id) {
         ToolResponseDTO tool = new ToolResponseDTO(toolQueryService.getToolDetails(id));
         return ResponseEntity.ok(tool);
     }
+
+
+    //dica, use o preAuthorization com o hasAuthoritie para bloquear a rota, caso for criar uma permission,
+    //Apenas coloque string inline no seu controller com o @preAuthoritie(hasAuthoritie("permission")) posteriormente
+    // implemento as permissions no shared.
 }
